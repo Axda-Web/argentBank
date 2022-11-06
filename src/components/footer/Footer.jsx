f6aed3f298d007a6ca20050d2e4f49964ff9b944
Alyx DARENNE <alyx.darenne@deptagency.com>
2022-11-06 17:56:14 +0100

    Add Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StyledFooter from './Footer.styled'
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </StyledFooter>
+  )
+}
+
+export default Footer
