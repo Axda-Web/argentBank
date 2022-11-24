8ae7c228da6302c2e6b4619d7cb9aca36ff48220
Alyx DARENNE <alyx.darenne@deptagency.com>
2022-11-24 08:56:23 +0100

    Add Form logic

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,29 +1,70 @@
-import React from 'react'
-import StyledForm from './Form.styled'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import StyledForm from "./Form.styled";
+import { Link } from "react-router-dom";
+import axios from "../../utils/axios";
 
-import Button from '../button'
+import Button from "../button";
+
+import { useSelector, useDispatch } from "react-redux";
+import { setUserName, setPassword, fetchToken} from "../../features/auth-slice/authSlice";
+import { selectPassword, selectUserName, selectToken, selectStatus} from "../../features/auth-slice/authSlice";
+
 
 const Form = () => {
+
+  const dispatch = useDispatch()
+  const userName = useSelector(selectUserName)
+  const password = useSelector(selectPassword)
+  const token = useSelector(selectToken)
+  const status = useSelector(selectStatus)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    name === 'username' ? dispatch(setUserName(value)) : dispatch(setPassword(value))
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("form submited");
+    try {
+      dispatch(fetchToken({email: userName, password}))
+      // const response = await axios.post('/login', { email: username, password })
+      // console.log(response)
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
   return (
-    <StyledForm>
+    <StyledForm onSubmit={handleSubmit}>
       <div className="input-wrapper">
-        <label for="username">Username</label>
-        <input type="text" id="username" />
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={userName}
+          onChange={handleChange}
+        />
       </div>
       <div className="input-wrapper">
-        <label for="password">Password</label>
-        <input type="password" id="password" />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
       </div>
       <div className="input-remember">
         <input type="checkbox" id="remember-me" />
-        <label for="remember-me">Remember me</label>
+        <label htmlFor="remember-me">Remember me</label>
       </div>
-      <Link to='/profile'>
-        <Button className='sign-in-button'>Sign in</Button>
-      </Link>
-    </StyledForm>
-  )
-}
 
-export default Form
+      <Button className="sign-in-button">Sign in</Button>
+    </StyledForm>
+  );
+};
+
+export default Form;
