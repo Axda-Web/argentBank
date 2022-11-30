aa8fbe89cae05bd6e6674404c1ecd6408c245f81
Alyx DARENNE <alyx.darenne@deptagency.com>
2022-11-30 09:22:49 +0100

    Add Redux functionalities to the Profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,19 +1,108 @@
-import React from 'react'
+import { useEffect } from 'react'
 import StyledProfile from './Profile.styled'
 
-import Button from '../../components/button'
+import Loader from '../../components/loader'
+
+
 import AccountCardContainer from '../../components/accountCardContainer'
 
+import { useSelector, useDispatch } from "react-redux";
+import {  setFirstName,
+          setLastName,
+          toggleDisplayEditNameForm,
+          getUserData,
+          updateUserData,
+          selectFirstName,
+          selectLastName,
+          selectFirstNameEdit,
+          selectLastNameEdit,
+          selectDisplayEditNameForm,
+          selectGetUserDataStatus 
+        } from '../../features/profile-slice/profileSlice';
+
+import { selectToken } from '../../features/login-slice/loginSlice';
+
+
+
 const Profile = () => {
+
+  const dispatch = useDispatch()
+  const token = useSelector(selectToken)
+  const getUserDataStatus = useSelector(selectGetUserDataStatus)
+
+  useEffect( () => {
+    dispatch(getUserData(token))
+  }, [])
+
+  const firstName = useSelector(selectFirstName)
+  const lastName = useSelector(selectLastName)
+  const firstNameEdit = useSelector(selectFirstNameEdit)
+  const lastNameEdit = useSelector(selectLastNameEdit)
+  const displayEditNameFrom = useSelector(selectDisplayEditNameForm)
+
+
+  const handleEditNameBtnClick = e => {
+    dispatch(toggleDisplayEditNameForm())
+    dispatch(setFirstName(''))
+    dispatch(setLastName(''))
+  }
+
+  const handleInputFieldChange = e => {
+    const { name, value } = e.target;
+    name === 'firstName' ? dispatch(setFirstName(value)) : dispatch(setLastName(value));
+  }
+
+  const handleSaveNameClick = e => {
+    try {
+      dispatch(updateUserData({ data: { firstName: firstNameEdit, lastName: lastNameEdit }, token}))
+      dispatch(toggleDisplayEditNameForm())
+      dispatch(setFirstName(''))
+      dispatch(setLastName(''))
+    } catch (err) {
+      console.log(err.response)
+    }
+  }
+
   return (
-    <StyledProfile>
-      <div className="header">
-        <h1>Welcome back<br />Tony Jarvis!</h1>
-        <Button className="edit-button">Edit Name</Button>
-      </div>
-      <h2 className="sr-only">Accounts</h2>
-      <AccountCardContainer />
-    </StyledProfile>
+    <> 
+      { getUserDataStatus === 'OK'
+      ? (<StyledProfile>
+          <div className="header">
+            <h1>Welcome back<br />{firstName} {lastName}</h1>
+            {displayEditNameFrom ? 
+              (<>
+                <div className="input-wrapper">
+                  <input
+                    className='profile-input'
+                    type="text"
+                    name="firstName"
+                    placeholder={firstName}
+                    value={firstNameEdit}
+                    onChange={handleInputFieldChange}
+                  />
+                  <input
+                    className='profile-input'
+                    type="text"
+                    name="lastName"
+                    placeholder={lastName}
+                    value={lastNameEdit}
+                    onChange={handleInputFieldChange}
+                  />
+                </div>
+                <div className="btn-wrapper">
+                  <button onClick={handleSaveNameClick} className="btn">Save</button>
+                  <button onClick={handleEditNameBtnClick} className="btn">Cancel</button>
+                </div>
+              </>) :
+              <button className="btn" onClick={handleEditNameBtnClick}>Edit name</button>
+            }
+          </div>
+          <h2 className="sr-only">Accounts</h2>
+          <AccountCardContainer />
+        </StyledProfile>)
+      : (<Loader />)
+      }
+    </>
   )
 }
 
--- src/pages/profile/Profile.styled.js
+++ src/pages/profile/Profile.styled.js
@@ -7,6 +7,36 @@ const StyledProfile = styled.main`
     .header {
         color: #fff;
         margin-bottom: 2rem;
+        padding: 1rem 0;
+    }
+
+    .btn {
+        margin: 0 auto;
+    }
+
+    input {
+        padding: 5px;
+        font-size: 1.2rem;
+        margin-bottom: 1rem;
+    }
+
+    .input-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        column-gap: 1rem;
+    }
+
+
+    .btn-wrapper {
+        display: flex;
+        column-gap: 1rem;
+        justify-content: center;
+
+        .btn {
+            width: 100px;
+            margin: 0;
+        }
     }
 `
 
