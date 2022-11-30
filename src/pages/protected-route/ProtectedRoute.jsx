9cf9e297981dad9fbbc257939d94ffa939217c50
Alyx DARENNE <alyx.darenne@deptagency.com>
2022-11-30 09:23:14 +0100

    Update protected route

--- src/pages/protected-route/ProtectedRoute.jsx
+++ src/pages/protected-route/ProtectedRoute.jsx
@@ -1,9 +1,12 @@
 import { Outlet, Navigate } from 'react-router-dom'
+
 import { useSelector } from 'react-redux'
+import { selectToken} from "../../features/login-slice/loginSlice";
+
 
 const ProtectedRoute = () => {
 
-  let token = useSelector()
+  let token = useSelector(selectToken)
 
   return (
     token ? <Outlet/> : <Navigate to="/login"/>
